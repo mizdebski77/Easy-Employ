e3db24872533789bb7585f0e12101c8a285f1e09
Marcin Izdebski <mizdebski123@gmail.com>
2023-11-20 14:28:30 +0100

    Carer menu removed

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -4,18 +4,17 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { WelcomePage } from "../features/WelcomePage/welcomePage";
 import { LogIn } from "../features/LogIn/logIn";
 import { MyCarrer } from "../features/MyCarrer/myCarrer";
-import { CarrerMenu } from "../common/CarrerMenu/carrerMenu";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/HomeScreen" element={<WelcomePage />} />
-        {/* <Route path={"*"} element={<Navigate replace to="/HomeScreen" />}></Route> */}
+        <Route path={"*"} element={<Navigate replace to="/HomeScreen" />}></Route>
         <Route path="/Home" element={<><Navigation /><Home /></>} />
         <Route path="/Log-In" element={<><Navigation /><LogIn /></>} />
-        <Route path="/Carrer" element={<><CarrerMenu /><MyCarrer /></>} />
-        <Route path="/Carrer:link" element={<><CarrerMenu /><MyCarrer /></>} />
+        <Route path="/Carrer" element={<><Navigation /><MyCarrer /></>} />
+        <Route path="/Carrer:link" element={<><Navigation /><MyCarrer /></>} />
 
       </Routes>
     </BrowserRouter>
