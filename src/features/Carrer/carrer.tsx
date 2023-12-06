9bb84801fbeeee98246cd65d4c7ff7c4b3cd2359
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-06 16:12:03 +0100

    Carrer section added

---
+++ src/features/Carrer/carrer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title, Wrapper } from './styledCarrer';
+
+export const Carrer = () => {
+    return (
+        <Wrapper>
+            <Title> My Career</Title>
+        </Wrapper>
+    );
+};
+
