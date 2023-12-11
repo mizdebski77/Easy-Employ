790388e616ef885fb4aa4124c06f0d66a7bd1609
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-11 16:07:19 +0100

    Tiles To porfile section added

--- src/features/Home/CarrerSection/carrerSection.tsx
+++ src/features/Home/CarrerSection/carrerSection.tsx
@@ -1,22 +1,54 @@
 import React from 'react';
-import {  ProfileImg, ProfileSpan, ProfileTitle, ProfileWrapper, TextWrapper, Title, Wrapper } from './styledCarrerSection';
+import { CarrerImg, CarrerTile, ContentWrapper, ProfileImg, ProfileLink, ProfileSpan, ProfileTitle, ProfileWrapper, TextContainer, TextWrapper, TileSpan, TileTitle, TilesWrapper, Title, Wrapper } from './styledCarrerSection';
 import profile from '../../../common/Images/profile.png';
+import calculator from '../../../common/Images/Carrer/sallarycalculaor.jpg';
+import offers from '../../../common/Images/Carrer/joboffers.jpg';
+import cv from '../../../common/Images/Carrer/CVCreator.jpg';
 
 export const CarrerSection = () => {
     return (
         <Wrapper>
             <Title> My Carrer</Title>
+
             <ProfileWrapper>
-                <ProfileImg src={profile} />
-
-                <TextWrapper>
-                    <ProfileTitle>My Profile</ProfileTitle>
-                    <ProfileSpan>- simpler application</ProfileSpan>
-                    <ProfileSpan>- faster access to recruitment documents</ProfileSpan>
-                    <ProfileSpan>- automatic completion of questions from employers </ProfileSpan>
-                </TextWrapper>
-
+                <ContentWrapper>
+                    <ProfileImg src={profile} />
+                    <TextWrapper>
+                        <ProfileTitle>My Profile</ProfileTitle>
+                        <ProfileSpan>- simpler application</ProfileSpan>
+                        <ProfileSpan>- faster access to recruitment documents</ProfileSpan>
+                        <ProfileSpan>- automatic completion of questions from employers </ProfileSpan>
+                    </TextWrapper>
+                </ContentWrapper>
+                <ProfileLink to="/Profile">Go to profile </ProfileLink>
             </ProfileWrapper>
+
+            <TilesWrapper>
+                <CarrerTile>
+                    <CarrerImg src={cv} />
+                    <TextContainer>
+                        <TileTitle>Salary Calculator</TileTitle>
+                        <TileSpan>Create your CV in a few simple steps with our CV creator</TileSpan>
+                    </TextContainer>
+                </CarrerTile>
+
+                <CarrerTile>
+                    <CarrerImg src={offers} />
+                    <TextContainer>
+                        <TileTitle>Salary Calculator</TileTitle>
+                        <TileSpan>Check the latest job offers and find your dream job</TileSpan>
+                    </TextContainer>
+                </CarrerTile>
+
+                <CarrerTile>
+                    <CarrerImg src={calculator} />
+                    <TextContainer>
+                        <TileTitle>Salary Calculator</TileTitle>
+                        <TileSpan>Calculate net and gross earnings for different types of contracts.</TileSpan>
+                    </TextContainer>
+                </CarrerTile>
+            </TilesWrapper>
+
         </Wrapper>
     );
 };
--- src/features/Home/CarrerSection/styledCarrerSection.ts
+++ src/features/Home/CarrerSection/styledCarrerSection.ts
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 export const Wrapper = styled.section`
@@ -15,34 +16,93 @@ export const Title = styled.h1`
     margin-bottom: 80px;
 `;
 
+export const ContentWrapper = styled.div`
+    display: flex;
+    gap: 40px;
+    position: relative;
+`;
+
 export const ProfileWrapper = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     gap: 40px;
     align-items: center;
+    background: white;
+    padding: 20px 80px 20px;
+    border-radius: 20px;
 `;
 
 export const TextWrapper = styled.div`
-
+    padding-left: 480px;
 `;
 
 export const ProfileTitle = styled.h2`
     font-size: 40px;
-    color: white;
+    color: ${({ theme }) => theme.color.fontMain};;
     font-weight: normal;
 `;
 
 export const ProfileSpan = styled.p`
-    color: white;
+    color: ${({ theme }) => theme.color.fontMain};;
 
-`;  
+`;
 
 export const ProfileImg = styled.img`
-    width: 200px;
-    height: 200px;
-    display: block;
+  position: absolute;
+  top: -80px; 
+  left: 0;
+  z-index: 1; 
+  width: 400px;
+`;
+
+export const ProfileLink = styled(Link)`
+    background: ${({ theme }) => theme.color.fontMain};
+    padding: 12px 48px;
+    color: ${({ theme }) => theme.color.secondColor};
+    text-decoration: none;
+    font-size: 18px;
+    border-radius: 20px;
+    transition:0.3s;
+
+    &:hover {
+        background: ${({ theme }) => theme.color.fontMainHover};
+    };
+
+    &:active {
+        background: ${({ theme }) => theme.color.fontMainActive};
+    };
+
+`;
+
+export const TilesWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-top: 40px;
+    gap: 40px;
 `;
 
 export const CarrerTile = styled.div`
+    background: white;
+    border-radius: 20px;
+`;
 
+export const CarrerImg = styled.img`
+    width: 100%;
+    border-radius: 20px;
+`;
+
+export const TextContainer = styled.div`
+    padding: 20px;
+`;
+
+export const TileTitle = styled.h3`
+    font-size: 28px;
+    text-align: center;
+    margin: 0 0 20px 0;
+    font-weight: normal;
+    color:${({ theme }) => theme.color.fontMain}
+`;
+
+export const TileSpan = styled.span`
+    font-size: 20px;
 `;
