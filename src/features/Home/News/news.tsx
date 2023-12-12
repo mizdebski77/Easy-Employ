f722effbad1a1ebbbbfc3fd44c70cf839dc62bb8
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-12 16:10:43 +0100

    Components to news elements added

--- src/features/Home/News/news.tsx
+++ src/features/Home/News/news.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
-import { Title, Wrapper } from './styledNews';
+import { ReadMore, Tile, TileArticle, TileContent, TileImage, TileTitle, TileWrapper, Title, Wrapper } from './styledNews';
+import ex from '../../../common/Images/Carrer/CVCreator.jpg';
 
 export const News = () => {
     return (
         <Wrapper>
             <Title>The most important news from the business world in one place</Title>
+            <TileWrapper>
+
+                <Tile>
+                    <TileImage src={ex} />
+                    <TileContent>
+                        <TileTitle>News one two three</TileTitle>
+                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
+                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                    </TileContent>
+                </Tile>
+
+                <Tile>
+                    <TileImage src={ex} />
+                    <TileContent>
+                        <TileTitle>News one two three</TileTitle>
+                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
+                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                    </TileContent>
+                </Tile>
+
+                <Tile>
+                    <TileImage src={ex} />
+                    <TileContent>
+                        <TileTitle>News one two three</TileTitle>
+                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
+                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                    </TileContent>
+                </Tile>
+
+                <Tile>
+                    <TileImage src={ex} />
+                    <TileContent>
+                        <TileTitle>News one two three</TileTitle>
+                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
+                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                    </TileContent>
+                </Tile>
+
+                <Tile>
+                    <TileImage src={ex} />
+                    <TileContent>
+                        <TileTitle>News one two three</TileTitle>
+                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
+                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                    </TileContent>
+                </Tile>
+
+                <Tile>
+                    <TileImage src={ex} />
+                    <TileContent>
+                        <TileTitle>News one two three</TileTitle>
+                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
+                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                    </TileContent>
+                </Tile>
+
+            </TileWrapper>
         </Wrapper>
     );
 };
--- src/features/Home/News/styledNews.ts
+++ src/features/Home/News/styledNews.ts
@@ -1,28 +1,65 @@
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 export const Wrapper = styled.section`
     margin-top: 80px;
 `;
 
-export const Title  = styled.h1`
+export const Title = styled.h1`
     font-size: 54px;
     font-weight: normal;
-    color: ${({theme}) => theme.color.fontMain};
+    color: ${({ theme }) => theme.color.fontMain};
     text-align: center;
 `;
 
 export const TileWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 40px;
+`;
 
+export const Tile = styled.div`
+    background: ${({ theme }) => theme.color.thirdColor};
+    box-shadow: 0 0 10px ${({ theme }) => theme.color.fontSecond};
+    border-radius: 20px;
+    transition: 0.3s;
+
+    &:hover {
+        box-shadow: 0 0 10px ${({ theme }) => theme.color.fontMain};
+    }
 `;
 
 export const TileImage = styled.img`
+    max-width: 600px;
+    width: 100%;
+    border-radius: 20px 20px 0 0;
+`;
 
+export const TileContent = styled.div`
+    padding: 20px;
 `;
 
 export const TileTitle = styled.h2`
-
+    font-size: 32px;
+    font-weight: normal;
+    color: ${({ theme }) => theme.color.fontMain};
+    text-align: center;
 `;
 
-export const TileArticle = styled.span`
+export const TileArticle = styled.article`
+    font-size: 20px;
+    text-align: justify;
+`;
 
+export const ReadMore = styled(Link)`
+    color: ${({ theme }) => theme.color.fontMain};
+    font-size: 18px;
+    text-decoration: none;
+    float: right;
+    padding: 20px 0 20px 20px;
+    transition: 0.3s;
+
+    &:hover {
+        transform: scale(1.05);
+    }
 `;
