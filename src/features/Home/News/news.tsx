d198ee0ebbb1c1142b1c5dac652b01a4e2867f74
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-14 12:28:43 +0100

    Fetch news function added

--- src/features/Home/News/news.tsx
+++ src/features/Home/News/news.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { ReadMore, Tile, TileArticle, TileContent, TileImage, TileTitle, TileWrapper, Title, Wrapper } from './styledNews';
 import ex from '../../../common/Images/Carrer/CVCreator.jpg';
+import { useQuery } from '@tanstack/react-query';
 
 export const News = () => {
+
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['news'],
+        queryFn: () => fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=1b6e99a4f4244702bd1caf4f3fd8e680")
+            .then((response: Response) => response.json())
+    });
+
+        console.log(data);
+
+
+
+
     return (
         <Wrapper>
             <Title>The most important news from the business world in one place</Title>
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,24 @@ import App from './core/App';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './core/globalStyles';
 import { theme } from './core/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <App />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <ReactQueryDevtools />
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
