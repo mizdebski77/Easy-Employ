9571281ffad06d7aab47a5be683ba1e09018cba8
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-15 08:45:17 +0100

    Data map added

---
+++ src/core/interface.ts
@@ -0,0 +1,9 @@
+export interface Article {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    title: string;
+    url: string;
+    urlToImage: string;
+};
--- src/features/Home/News/news.tsx
+++ src/features/Home/News/news.tsx
@@ -2,81 +2,34 @@ import React from 'react';
 import { ReadMore, Tile, TileArticle, TileContent, TileImage, TileTitle, TileWrapper, Title, Wrapper } from './styledNews';
 import ex from '../../../common/Images/Carrer/CVCreator.jpg';
 import { useQuery } from '@tanstack/react-query';
+import { Article } from '../../../core/interface';
 
 export const News = () => {
-
     const { data, isLoading, error } = useQuery({
         queryKey: ['news'],
         queryFn: () => fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=1b6e99a4f4244702bd1caf4f3fd8e680")
             .then((response: Response) => response.json())
     });
 
-        console.log(data);
-
-
-
+    console.log(data?.articles);
 
     return (
         <Wrapper>
             <Title>The most important news from the business world in one place</Title>
             <TileWrapper>
-
-                <Tile>
-                    <TileImage src={ex} />
-                    <TileContent>
-                        <TileTitle>News one two three</TileTitle>
-                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
-                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
-                    </TileContent>
-                </Tile>
-
-                <Tile>
-                    <TileImage src={ex} />
-                    <TileContent>
-                        <TileTitle>News one two three</TileTitle>
-                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
-                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
-                    </TileContent>
-                </Tile>
-
-                <Tile>
-                    <TileImage src={ex} />
-                    <TileContent>
-                        <TileTitle>News one two three</TileTitle>
-                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
-                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
-                    </TileContent>
-                </Tile>
-
-                <Tile>
-                    <TileImage src={ex} />
-                    <TileContent>
-                        <TileTitle>News one two three</TileTitle>
-                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
-                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
-                    </TileContent>
-                </Tile>
-
-                <Tile>
-                    <TileImage src={ex} />
-                    <TileContent>
-                        <TileTitle>News one two three</TileTitle>
-                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
-                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
-                    </TileContent>
-                </Tile>
-
-                <Tile>
-                    <TileImage src={ex} />
-                    <TileContent>
-                        <TileTitle>News one two three</TileTitle>
-                        <TileArticle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis dolor dolore facere ullam tempore, beatae vel iste eum pariatur temporibus qui eos magnam eaque aspernatur porro, neque quidem cupiditate.</TileArticle>
-                        <ReadMore to='/Article'> Read more 🡢 </ReadMore>
-                    </TileContent>
-                </Tile>
-
+                {isLoading ? <div>Czekaj!</div> : (
+                    data?.articles.map((article: Article) => (
+                        <Tile key={article.title}>
+                            <TileImage src={ex} />
+                            <TileContent>
+                                <TileTitle>{article.title}</TileTitle>
+                                <TileArticle>{article.content}</TileArticle>
+                                <ReadMore to='/Article'> Read more 🡢 </ReadMore>
+                            </TileContent>
+                        </Tile>
+                    ))
+                )}
             </TileWrapper>
         </Wrapper>
     );
 };
-
