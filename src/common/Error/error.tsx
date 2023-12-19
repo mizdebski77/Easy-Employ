7f40dc8007a2692a403eeeafb78da0e2b087a9d2
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-19 10:19:05 +0100

    Error added

--- src/common/Error/error.tsx
+++ src/common/Error/error.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Wrapper } from './styledError';
+import { Image, Title, Wrapper } from './styledError';
+import error from './error.svg';
 
 export const Error = () => {
     return (
         <Wrapper>
-
+            <Image src={error} />
+            <Title>Ups.. Something went wrong. Try again later</Title>
         </Wrapper>
     );
 };
--- src/common/Error/styledError.ts
+++ src/common/Error/styledError.ts
@@ -1,5 +1,34 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 
-export const Wrapper =styled.div`
-    
-`;  
+export const Wrapper = styled.div`
+    display: flex;
+    margin-top: 10%;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: start;
+    align-items: center;
+    width: 100%;
+    min-height: 100vh;
+`;
+
+export const Title = styled.h1`
+    font-size: 32px;
+    font-weight: normal;
+    text-align: center;
+    color: ${({ theme }) => theme.color.fontMain};
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.thirdBp}px) {
+        font-size: 20px;
+    };
+`;
+
+export const Image = styled.img`
+    margin: 0 auto ;
+    width: 150px;
+    height: 150px;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.thirdBp}px) {
+        width: 100px;
+        height: 100px;    
+    };
+`;
