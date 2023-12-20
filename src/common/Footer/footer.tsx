248a2c76b6a8ab3fbc56f22f0a88ecc1df0fe65d
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-20 11:04:03 +0100

    Footer wrapper added

---
+++ src/common/Footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Wrapper } from './styledFooter';
+
+export const Footer = () => {
+    return (
+        <Wrapper>
+            xd
+        </Wrapper>
+    );
+};
+
