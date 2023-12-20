e7ab45aede713328531476edf1ac605aef2a5db8
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-20 11:23:56 +0100

    Logo to footer added

--- src/common/Footer/footer.tsx
+++ src/common/Footer/footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Wrapper } from './styledFooter';
+import { Logo, LogoFirstLetters, LogoText, LogoWrapper, Wrapper } from './styledFooter';
+import logo from '../Images/logo.png';
+
 
 export const Footer = () => {
     return (
         <Wrapper>
-            xd
+            <LogoWrapper to='/Home'>
+                <Logo src={logo} />
+                <LogoText>
+                    <LogoFirstLetters>E</LogoFirstLetters>asy
+                    <LogoFirstLetters> E</LogoFirstLetters>mploy
+                </LogoText>
+            </LogoWrapper>
         </Wrapper>
     );
 };
--- src/common/Footer/styledFooter.ts
+++ src/common/Footer/styledFooter.ts
@@ -1,7 +1,48 @@
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 export const Wrapper = styled.footer`
     background: ${({ theme }) => theme.color.thirdColor};
     padding: 120px 40px;
     margin-top: 40px;
-`;  
+`;
+
+export const LogoWrapper = styled(Link)`
+    display: flex;
+    align-items: center;
+    gap:10px;
+    text-decoration: none;
+`;
+
+export const Logo = styled.img`
+    max-width: 80px;
+`;
+
+export const LogoText = styled.span`
+    font-size: 40px;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.color.fontSecond};
+    font-weight: normal;
+    letter-spacing: 2px;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.thirdBp}px){
+        font-size: 20px;
+    };
+`;
+
+export const LogoFirstLetters = styled.span`
+    -webkit-text-stroke:1px ${({ theme }) => theme.color.fontMain};
+    -webkit-text-fill-color:transparent;
+`;
+
+export const ToolsWrapper = styled.div`
+
+`;
+
+export const AppWrapper = styled.div`
+
+`;
+
+export const IformationsWrapper = styled.div`
+
+`;
