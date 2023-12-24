c61b86e891f5fe1bdb787a39b8e936349952d888
Marcin Izdebski <mizdebski123@gmail.com>
2023-12-24 11:18:37 +0100

    Unused elements from login panel removed

--- src/features/LogIn/logIn.tsx
+++ src/features/LogIn/logIn.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Form, FormContainer, FormTitle, FormsWrapper, Input, Title, TitleSpan, Wrapper } from './styledLogIn';
-import { motion } from 'framer-motion';
 
 export const LogIn = () => {
 
