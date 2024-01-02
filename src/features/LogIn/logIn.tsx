4172253995a3a1ac143f05fe6c0aa6afc0eb63c6
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-02 12:49:24 +0100

    Unused elements removed

--- src/features/LogIn/logIn.tsx
+++ src/features/LogIn/logIn.tsx
@@ -1,37 +1,13 @@
 import React from 'react';
-import { Button, Form, FormContainer, FormTitle, FormsWrapper, Input, Title, TitleSpan, Wrapper } from './styledLogIn';
+import { Button, Form, FormContainer, FormTitle, FormsWrapper, Input, LoginTitle, LoginWrapper, Title, TitleSpan, Wrapper } from './styledLogIn';
 
 export const LogIn = () => {
 
     return (
         <Wrapper>
-            <Title>Log in to our application
-                <TitleSpan> Or </TitleSpan>
-                create an account
-            </Title>
-
-            <FormsWrapper>
-                <FormContainer>
-                    <FormTitle>Log-In</FormTitle>
-                    <Form>
-                        <Input placeholder='E-Mail' type='email' required />
-                        <Input placeholder='Password' type='password' required />
-                        <Button>Log-In</Button>
-                    </Form>
-                </FormContainer>
-
-                <FormContainer>
-                    <FormTitle>Create an account</FormTitle>
-                    <Form>
-                        <Input placeholder='Name' type='name' required />
-                        <Input placeholder='Surname' type='name' required />
-                        <Input placeholder='E-Mail' type='email' required />
-                        <Input placeholder='Password' type='password' required />
-                        <Input placeholder='Reapeat password' type='password' required />
-                        <Button>Create an account</Button>
-                    </Form>
-                </FormContainer>
-            </FormsWrapper>
+            <LoginWrapper>
+                <LoginTitle>Login to Your Account</LoginTitle>
+            </LoginWrapper>
 
         </Wrapper >
     );
--- src/features/LogIn/styledLogIn.ts
+++ src/features/LogIn/styledLogIn.ts
@@ -12,98 +12,15 @@ export const Wrapper = styled.section`
     };
 `;
 
-export const Title = styled.h1`
-    font-size: 54px;
-    font-weight: normal;
-    color: ${({ theme }) => theme.color.fontSecond};
-    text-align: center;
+export const LoginWrapper = styled.div`
 
-    @media (max-width: ${({ theme }) => theme.breakPoint.secondBp}px){
-        font-size: 28px;
-    };
 `;
 
-export const TitleSpan = styled.span`
-    color: ${({ theme }) => theme.color.fontMain};
+export const LoginTitle = styled.h1`
+
 `;
 
-export const FormsWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 40px;
-    max-width: 1200px;
-    width: 100%;
+export const RegisterWrapper = styled.div`
 
-    @media (max-width: ${({ theme }) => theme.breakPoint.secondBp}px){
-        grid-template-columns: 1fr;
-    };
 `;
 
-export const FormTitle = styled.h2`
-    text-align: center;
-    color: ${({ theme }) => theme.color.fontMain};
-    font-weight: normal;
-    font-size: 36px;
-    margin: 20px 10px;
-
-    @media (max-width: ${({ theme }) => theme.breakPoint.secondBp}px){
-        font-size: 20px;
-    };
-`;
-export const FormContainer = styled.div`
-    max-width: 1200px;
-    width: 100%;
-`;
-
-export const Form = styled.form`
-    background: transparent;
-    border: 2px solid ${({ theme }) => theme.color.thirdColor};
-    border-radius: 10px;
-    padding: 40px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background: ${({ theme }) => theme.color.secondColor};
-
-    @media (max-width: ${({ theme }) => theme.breakPoint.thirdBp}px){
-        padding: 40px 12px;
-    };
-`;
-
-export const Input = styled.input`
-    width: 100%;
-    padding: 10px;
-    border-radius: 4px;
-    background: ${({ theme }) => theme.color.thirdColor};;
-    border: 2px solid white;
-    font-size: 20px;
-    outline: none;
-    color: ${({ theme }) => theme.color.fontSecond};
-    margin-bottom: 20px;
-
-    @media (max-width: ${({ theme }) => theme.breakPoint.thirdBp}px){
-        font-size: 16px;
-        border: 1px solid white;
-    };
-`;
-
-export const Button = styled.button`
-    margin: 0 auto;
-    cursor: pointer;
-    padding: 8px 80px;
-    font-size: 20px;
-    border-radius: 4px;
-    background: ${({ theme }) => theme.color.fontMain};
-    color: ${({ theme }) => theme.color.secondColor};
-    transition: 0.3s;
-    border: none;
-
-    &:hover {
-        background: ${({ theme }) => theme.color.fontMainHover};;
-    };
-
-    @media (max-width: ${({ theme }) => theme.breakPoint.thirdBp}px){
-        padding: 8px 40px;
-        font-size: 16px;
-    };
-`;
