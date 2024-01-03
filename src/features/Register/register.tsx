e12a8fde377d4779b9d7c7cb61dc715204a51a10
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-03 12:42:37 +0100

    Icons to register panel added

--- src/features/Register/register.tsx
+++ src/features/Register/register.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Wrapper } from './styledRegister';
+import { Icon, IconContainer, IconsWrapper, RegisterTitle, SignUpPanel, Wrapper } from './styledRegister';
+import fb from '../../common/Images/SocialMedia/fb.png';
+import link from '../../common/Images/SocialMedia/link.png';
+import google from '../../common/Images/SocialMedia/google.png';
 
 export const Register = () => {
     return (
         <Wrapper>
+            <SignUpPanel>
+                <RegisterTitle>Create account</RegisterTitle>
+                <IconsWrapper>
+                    <IconContainer>
+                        <Icon src={fb} />
+                    </IconContainer>
+                    <IconContainer>
+                        <Icon src={link} />
+                    </IconContainer>
+                    <IconContainer>
+                        <Icon src={google} />
+                    </IconContainer>
 
+                </IconsWrapper>
+            </SignUpPanel>
         </Wrapper>
     );
 };
--- src/features/Register/styledRegister.ts
+++ src/features/Register/styledRegister.ts
@@ -1,7 +1,48 @@
 import { styled } from "styled-components";
 
-export const Wrapper =styled.section`
+export const Wrapper = styled.section`
     min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const SignUpPanel = styled.div`
+    background: ${({ theme }) => theme.color.secondColor};
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+`;
+
+export const RegisterTitle = styled.h1`
+    font-weight: normal;
+    color: ${({theme}) => theme.color.fontSecond};
+    text-align: center;
+    font-size: 48px;
+`;
+
+export const IconsWrapper = styled.div`
+
+`;
+
+export const IconContainer = styled.div`
+
+`;
+
+export const Icon = styled.img`
+
+`;
+
+export const IconSpan = styled.span`
+
+`;
+
+export const Span = styled.span`
+
+`;
+
+export const CreateAccountButton = styled.button`
+
 `;
 
 export const Form = styled.form`
