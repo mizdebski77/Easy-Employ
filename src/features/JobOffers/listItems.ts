c1f1628c9017671641b0fec98c1e15c3bcea8b6a
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-12 12:26:22 +0100

    Other categories added to listItems

--- src/features/JobOffers/listItems.ts
+++ src/features/JobOffers/listItems.ts
@@ -23,4 +23,41 @@ export const Categories = [
             { id: 12, text: "Game Dev" },
         ],
     },
+
+    {
+        id: 3,
+        title: "Position Level",
+        items: [
+            { id: 13, text: "Trainee" },
+            { id: 14, text: "Junior" },
+            { id: 15, text: "Mid-Level" },
+            { id: 16, text: "Senior" },
+            { id: 17, text: "Lead" },
+            { id: 18, text: "Manager" },
+            { id: 19, text: "Director" },
+        ],
+    },
+    {
+        id: 4,
+        title: "Employment Type",
+        items: [
+            { id: 20, text: "Employment contract" },
+            { id: 21, text: "Work contract" },
+            { id: 22, text: "Contract of mandate" },
+            { id: 23, text: "B2b contract" },
+            { id: 24, text: "Internship" },
+            { id: 25, text: "Internship / apprenticeship contract" },
+        ],
+    },
+
+    {
+        id: 5,
+        title: "Working dimension",
+        items: [
+            { id: 26, text: "Full-Time" },
+            { id: 27, text: "Part-Time" },
+            { id: 28, text: "Temporary" },
+
+        ],
+    },
 ];
