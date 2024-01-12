2f92755c6b24bbdf8a4c3e2b50f0abcd8bee9276
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-12 12:06:57 +0100

    Unused elements in joboffers removed

--- src/features/JobOffers/jobOffers.tsx
+++ src/features/JobOffers/jobOffers.tsx
@@ -47,8 +47,6 @@ export const JobOffers = () => {
     };
 
 
-
-
     return (
         <Wrapper>
             <Title>
--- src/features/JobOffers/listItems.ts
+++ src/features/JobOffers/listItems.ts
@@ -1,24 +1,26 @@
 export const Categories = [
     {
+        id: 1,
         title: "Operating mode",
         items: [
-            { text: 'Hybrid Work' },
-            { text: 'Remote Work' },
-            { text: 'Stationary Work' },
-            { text: 'Mobile Work' },
+            { id: 1, text: 'Hybrid Work' },
+            { id: 2, text: 'Remote Work' },
+            { id: 3, text: 'Stationary Work' },
+            { id: 4, text: 'Mobile Work' },
         ],
     },
     {
+        id: 2,
         title: "Specializations",
         items: [
-            { text: "Backend" },
-            { text: "Frontend" },
-            { text: "Full-Stack" },
-            { text: "Mobile" },
-            { text: "DevOps" },
-            { text: "Testing" },
-            { text: "Security" },
-            { text: "Game Dev" },
+            { id: 5, text: "Backend" },
+            { id: 6, text: "Frontend" },
+            { id: 7, text: "Full-Stack" },
+            { id: 8, text: "Mobile" },
+            { id: 9, text: "DevOps" },
+            { id: 10, text: "Testing" },
+            { id: 11, text: "Security" },
+            { id: 12, text: "Game Dev" },
         ],
     },
 ];
