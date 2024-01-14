3f5de58bfa17f94e4d319c3cb9ce6218db42aef3
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-14 10:37:22 +0100

    Unused element in styledlogin removed

--- src/features/LogIn/styledLogIn.ts
+++ src/features/LogIn/styledLogIn.ts
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { css, styled } from "styled-components";
+import { styled } from "styled-components";
 
 export const Wrapper = styled.section`
     display: grid;
