1fcd5c89a6d5795f05c937c9f1e8667a1be6c486
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-19 10:40:52 +0100

    CV creator elements to styledcreator added

--- src/features/Creator/creator.tsx
+++ src/features/Creator/creator.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Title, Wrapper } from './styledCreator';
+import { CVPreview, ContentWrapper, FormsWrapper, Title, Wrapper } from './styledCreator';
 
 export const Creator = () => {
     return (
         <Wrapper>
             <Title>CV Creator</Title>
+
+            <ContentWrapper>
+                <FormsWrapper>xd</FormsWrapper>
+                <CVPreview></CVPreview>
+            </ContentWrapper>
         </Wrapper>
     );
 };
--- src/features/Creator/styledCreator.ts
+++ src/features/Creator/styledCreator.ts
@@ -10,7 +10,25 @@ export const Wrapper = styled.section`
 export const Title = styled.h1`
     font-size: 54px;
     font-weight: normal;
-    color: ${({theme}) => theme.color.fontMain};
+    color: ${({ theme }) => theme.color.fontMain};
     text-align: center;
+`;
+
+export const ContentWrapper = styled.div`
+    max-width: 1500px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 20px;
+`;
+
+export const FormsWrapper = styled.div`
+    background: ${({ theme }) => theme.color.secondColor};
+    padding: 40px 20px;
+`;
+
+export const CVPreview = styled.div`
+    background: ${({ theme }) => theme.color.secondColor};
+    padding: 40px 20px;
 `;
 
