3121c34d265f4fc5efa5dcc5204c1f765ac14d1c
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-24 10:39:08 +0100

    exports in keywordslice added

--- src/features/JobOffers/jobOffersSlice.ts
+++ src/features/JobOffers/jobOffersSlice.ts
@@ -9,7 +9,6 @@ const keyWordsSlice = createSlice({
     name: "keyWords",
     initialState: {
         keyWords: [],
-
     } as KeyWordsState,
 
     reducers: {
@@ -19,3 +18,10 @@ const keyWordsSlice = createSlice({
 
     }
 });
+
+export const selectKeyWordsState = (state: KeyWordsState) => state;
+export const SelectKeyWords = (state: KeyWordsState) => state.keyWords;
+
+export const { addKeyWord } = keyWordsSlice.actions;
+
+export default keyWordsSlice.reducer;
