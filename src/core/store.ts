b18070829c77b22c15b20a43ec78fbc8406765ba
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-24 10:50:11 +0100

    keyWordsReducer to store added

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import keyWordsReducer from '../features/JobOffers/jobOffersSlice';
 
 const store = configureStore({
     reducer: {
-
+        keyWords: keyWordsReducer,
     },
 });
 
