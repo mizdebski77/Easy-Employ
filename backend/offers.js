fa0ccdb2c9d4e749ed5b031da1cea167f909070d
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-29 15:38:40 +0100

    Logo links to offers array added

--- backend/offers.js
+++ backend/offers.js
@@ -5,7 +5,8 @@ const offers = [
         "gross_salary": "$45,000 - $55,000",
         "work_type": "Hybrid",
         "company_name": "Tech Innovators",
-        "location": "Warsaw, Poland"
+        "location": "Warsaw, Poland",
+        "logo": "https://w7.pngwing.com/pngs/383/458/png-transparent-ios-stocks-apple-stocks-stocks-ios-stocks-logo-apple-stocks-logo-ios-stocks-application-logo-3d-icon-thumbnail.png"
     },
     {
         "id": "g8a2s1df",
@@ -13,7 +14,8 @@ const offers = [
         "gross_salary": "$50,000 - $60,000",
         "work_type": "Remote",
         "company_name": "Data Insights Co.",
-        "location": "San Francisco, USA"
+        "location": "San Francisco, USA",
+        "logo": "https://w7.pngwing.com/pngs/671/818/png-transparent-logo-bear-bull-stock-market-thumbnail.png"
     },
     {
         "id": "k3l2j4h5",
@@ -21,7 +23,8 @@ const offers = [
         "gross_salary": "$60,000 - $75,000",
         "work_type": "On-site",
         "company_name": "CodeCraft Solutions",
-        "location": "Berlin, Germany"
+        "location": "Berlin, Germany",
+        "logo": "https://w7.pngwing.com/pngs/730/627/png-transparent-american-stock-exchange-hd-logo-thumbnail.png"
     },
     {
         "id": "o9t7y6a5",
@@ -29,7 +32,8 @@ const offers = [
         "gross_salary": "$55,000 - $65,000",
         "work_type": "Hybrid",
         "company_name": "DesignTech Studios",
-        "location": "London, UK"
+        "location": "London, UK",
+        "logo": "https://w7.pngwing.com/pngs/921/33/png-transparent-nyse-new-york-stock-exchange-hd-logo-thumbnail.png"
     },
     {
         "id": "p1q2r3z4",
@@ -37,7 +41,8 @@ const offers = [
         "gross_salary": "$70,000 - $85,000",
         "work_type": "Remote",
         "company_name": "SecureNet Solutions",
-        "location": "Singapore"
+        "location": "Singapore",
+        "logo": "https://w7.pngwing.com/pngs/596/640/png-transparent-strong-bull-stock-market-logo-thumbnail.png"
     },
     {
         "id": "x5y6z7a8",
@@ -45,7 +50,8 @@ const offers = [
         "gross_salary": "$80,000 - $95,000",
         "work_type": "On-site",
         "company_name": "AI Innovate Labs",
-        "location": "Toronto, Canada"
+        "location": "Toronto, Canada",
+        "logo": "https://w7.pngwing.com/pngs/758/156/png-transparent-london-stock-exchange-hd-logo-thumbnail.png"
     },
     {
         "id": "b2c3d4e5",
@@ -53,7 +59,8 @@ const offers = [
         "gross_salary": "$75,000 - $90,000",
         "work_type": "Hybrid",
         "company_name": "CloudOps Technologies",
-        "location": "Sydney, Australia"
+        "location": "Sydney, Australia",
+        "logo": "https://w7.pngwing.com/pngs/336/889/png-transparent-excavator-architectural-engineering-backhoe-machine-excavator-service-technic-logo-thumbnail.png"
     },
     {
         "id": "f6g7h8i9",
@@ -61,7 +68,8 @@ const offers = [
         "gross_salary": "$65,000 - $80,000",
         "work_type": "Remote",
         "company_name": "CodeMasters Inc.",
-        "location": "Bangalore, India"
+        "location": "Bangalore, India",
+        "logo": "https://w7.pngwing.com/pngs/786/126/png-transparent-logo-contracting-photography-logo-symbol-thumbnail.png"
     },
     {
         "id": "j1k2l3m4",
@@ -69,7 +77,8 @@ const offers = [
         "gross_salary": "$90,000 - $110,000",
         "work_type": "On-site",
         "company_name": "TechLeaders Solutions",
-        "location": "New York City, USA"
+        "location": "New York City, USA",
+        "logo": "https://w7.pngwing.com/pngs/532/457/png-transparent-black-dragon-logo-dragon-logo-dragon-logo-free-logo-design-template-head-wedding-logo-thumbnail.png"
     }
 ]
 
