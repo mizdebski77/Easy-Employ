250ee50a105e4ed18b42be3d2fd67171778a5cfd
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-29 14:44:07 +0100

    Port configuration added

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const offers = require("./offers");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send(offers)
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server running on port ${port}`));
