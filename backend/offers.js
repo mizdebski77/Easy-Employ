830697d63f00b233ba2a0ff690e151277c5606dc
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-29 15:03:20 +0100

    Offers id added

--- backend/offers.js
+++ backend/offers.js
@@ -1,6 +1,6 @@
-
 const offers = [
     {
+        "id": "23sa1sza",
         "position": "Junior Frontend Developer",
         "gross_salary": "$45,000 - $55,000",
         "work_type": "Hybrid",
@@ -8,6 +8,7 @@ const offers = [
         "location": "Warsaw, Poland"
     },
     {
+        "id": "g8a2s1df",
         "position": "Data Analyst",
         "gross_salary": "$50,000 - $60,000",
         "work_type": "Remote",
@@ -15,6 +16,7 @@ const offers = [
         "location": "San Francisco, USA"
     },
     {
+        "id": "k3l2j4h5",
         "position": "Backend Software Engineer",
         "gross_salary": "$60,000 - $75,000",
         "work_type": "On-site",
@@ -22,6 +24,7 @@ const offers = [
         "location": "Berlin, Germany"
     },
     {
+        "id": "o9t7y6a5",
         "position": "UX/UI Designer",
         "gross_salary": "$55,000 - $65,000",
         "work_type": "Hybrid",
@@ -29,6 +32,7 @@ const offers = [
         "location": "London, UK"
     },
     {
+        "id": "p1q2r3z4",
         "position": "Cybersecurity Analyst",
         "gross_salary": "$70,000 - $85,000",
         "work_type": "Remote",
@@ -36,6 +40,7 @@ const offers = [
         "location": "Singapore"
     },
     {
+        "id": "x5y6z7a8",
         "position": "Machine Learning Engineer",
         "gross_salary": "$80,000 - $95,000",
         "work_type": "On-site",
@@ -43,6 +48,7 @@ const offers = [
         "location": "Toronto, Canada"
     },
     {
+        "id": "b2c3d4e5",
         "position": "DevOps Specialist",
         "gross_salary": "$75,000 - $90,000",
         "work_type": "Hybrid",
@@ -50,6 +56,7 @@ const offers = [
         "location": "Sydney, Australia"
     },
     {
+        "id": "f6g7h8i9",
         "position": "Full Stack Developer",
         "gross_salary": "$65,000 - $80,000",
         "work_type": "Remote",
@@ -57,6 +64,7 @@ const offers = [
         "location": "Bangalore, India"
     },
     {
+        "id": "j1k2l3m4",
         "position": "IT Project Manager",
         "gross_salary": "$90,000 - $110,000",
         "work_type": "On-site",
