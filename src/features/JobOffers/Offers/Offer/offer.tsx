83617423d23cfe6b4e56a8932511473aa687694a
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-01 15:14:36 +0100

    Unused elements from offer section removed

--- src/features/JobOffers/Offers/Offer/offer.tsx
+++ src/features/JobOffers/Offers/Offer/offer.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {
-    Wrapper,
-} from './styledOffer';
+import { Wrapper } from './styledOffer';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { Loader } from '../../../../common/Loader/loader';
 import { Error } from '../../../../common/Error/error';
-import map from '../../../../common/Images/map.png';
 import money from '../../../../common/Images/SVG/money.svg';
 import location from '../../../../common/Images/SVG/location.svg';
 import tof from '../../../../common/Images/SVG/tof.svg';
@@ -27,32 +24,12 @@ export const Offer = () => {
         return null;
     }
 
-    const Informations = [
-        { text: `${data.gross_salary}`, src: money, title: 'Gross Sallary' },
-        { text: `${data.location}`, src: location, title: 'Location' },
-        { text: `${data.work_type}`, src: tof, title: 'Type Of Work' },
-    ];
-
     return (
         <Wrapper>
             {isLoading ? <Loader /> : error ? <Error /> : (
                 <>
                     <BasicInfo data={data} />
                     <Details />
-                    {/* 
-                    <DetailsContainer>
-                        <div>
-                            <InformationTile>
-                                <SectionTitle>Tech stack</SectionTitle>
-
-                            </InformationTile>
-                        </div>
-
-                        <MapWrapper>
-                            <Map src={map} />
-                        </MapWrapper>
-
-                    </DetailsContainer> */}
                 </>
             )}
         </Wrapper>
