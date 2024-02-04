63c299dad1e9306b24f8c811a942ade03d3e3440
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-04 12:02:46 +0100

    Checked boolean to categories items added

--- src/features/JobOffers/Filters/listItems.ts
+++ src/features/JobOffers/Filters/listItems.ts
@@ -2,7 +2,7 @@ export const Categories = [
     {
         id: 1,
         title: "Operating mode",
-        isExpand: false, // Add this property
+        isExpand: false,
         items: [
             { id: 1, text: 'Hybrid Work', checked: false },
             { id: 2, text: 'Remote Work', checked: false },
@@ -10,54 +10,51 @@ export const Categories = [
             { id: 4, text: 'Mobile Work', checked: false },
         ],
     },
-    // {
-    //     id: 2,
-    //     title: "Specializations",
-    //     items: [
-    //         { id: 5, text: "Backend" },
-    //         { id: 6, text: "Frontend" },
-    //         { id: 7, text: "Full-Stack" },
-    //         { id: 8, text: "Mobile" },
-    //         { id: 9, text: "DevOps" },
-    //         { id: 10, text: "Testing" },
-    //         { id: 11, text: "Security" },
-    //         { id: 12, text: "Game Dev" },
-    //     ],
-    // },
-
-    // {
-    //     id: 3,
-    //     title: "Position Level",
-    //     items: [
-    //         { id: 13, text: "Trainee" },
-    //         { id: 14, text: "Junior" },
-    //         { id: 15, text: "Mid-Level" },
-    //         { id: 16, text: "Senior" },
-    //         { id: 17, text: "Lead" },
-    //         { id: 18, text: "Manager" },
-    //         { id: 19, text: "Director" },
-    //     ],
-    // },
-    // {
-    //     id: 4,
-    //     title: "Employment Type",
-    //     items: [
-    //         { id: 20, text: "Employment contract" },
-    //         { id: 21, text: "Work contract" },
-    //         { id: 22, text: "Contract of mandate" },
-    //         { id: 23, text: "B2b contract" },
-    //         { id: 24, text: "Internship" },
-    //     ],
-    // },
-
-    // {
-    //     id: 5,
-    //     title: "Working dimension",
-    //     items: [
-    //         { id: 26, text: "Full-Time" },
-    //         { id: 27, text: "Part-Time" },
-    //         { id: 28, text: "Temporary" },
-
-    //     ],
-    // },
+    {
+        id: 2,
+        title: "Specializations",
+        items: [
+            { id: 5, text: "Backend", checked: false },
+            { id: 6, text: "Frontend", checked: false },
+            { id: 7, text: "Full-Stack", checked: false },
+            { id: 8, text: "Mobile", checked: false },
+            { id: 9, text: "DevOps", checked: false },
+            { id: 10, text: "Testing", checked: false },
+            { id: 11, text: "Security", checked: false },
+            { id: 12, text: "Game Dev", checked: false },
+        ],
+    },
+    {
+        id: 3,
+        title: "Position Level",
+        items: [
+            { id: 13, text: "Trainee", checked: false },
+            { id: 14, text: "Junior", checked: false },
+            { id: 15, text: "Mid-Level", checked: false },
+            { id: 16, text: "Senior", checked: false },
+            { id: 17, text: "Lead", checked: false },
+            { id: 18, text: "Manager", checked: false },
+            { id: 19, text: "Director", checked: false },
+        ],
+    },
+    {
+        id: 4,
+        title: "Employment Type",
+        items: [
+            { id: 20, text: "Employment contract", checked: false },
+            { id: 21, text: "Work contract", checked: false },
+            { id: 22, text: "Contract of mandate", checked: false },
+            { id: 23, text: "B2b contract", checked: false },
+            { id: 24, text: "Internship", checked: false },
+        ],
+    },
+    {
+        id: 5,
+        title: "Working dimension",
+        items: [
+            { id: 26, text: "Full-Time", checked: false },
+            { id: 27, text: "Part-Time", checked: false },
+            { id: 28, text: "Temporary", checked: false },
+        ],
+    },
 ];
