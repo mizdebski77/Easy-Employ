a3103185d4ea2d1b94b1b0693bdf28b116b4757c
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-29 15:51:22 +0100

    News section removed

--- src/features/Home/home.tsx
+++ src/features/Home/home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { CaptionNumber, Captions, Wrapper, Title, TitleWrapper } from './styledHome';
 import { OfferSection } from './OfferSection/offerSection';
 import { CarrerSection } from './CarrerSection/carrerSection';
-import { News } from './News/news';
 import { AppSection } from './AppSection/appSection';
 
 export const Home = () => {
@@ -15,7 +14,6 @@ export const Home = () => {
             </TitleWrapper>
             <OfferSection />
             <CarrerSection />
-            <News />
             <AppSection />
         </Wrapper>
     );
