179e47601d453191852249abc356e74163ed610e
Marcin Izdebski <mizdebski123@gmail.com>
2024-03-10 16:37:45 +0100

    Unused elements from calculator removed

--- src/features/Calculator/calculator.tsx
+++ src/features/Calculator/calculator.tsx
@@ -19,58 +19,6 @@ import {
 
 export const Calculator = () => {
 
-    const [selectedOption, setSelectedOption] = useState('');
-    const [monthlySalary, setMonthlySalary] = useState(0);
-    const [title, setTitle] = useState('Gross Net Consumption Calculator (2023)')
-    const [COE, setCOE] = useState(0);
-    const [MC, setMC] = useState(0);
-    const [CW, setCW] = useState(0);
-    const [B2B, setB2B] = useState(0);
-
-    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const unselectedOption = selectedOption === 'gross' ? 'net' : 'gross';
-        setTitle(`${monthlySalary} zł ${selectedOption} - check how much to ${unselectedOption}`);
-    };
-
-    const socialSecurityContributions = {
-        pension: monthlySalary * 0.0976,
-        rent: monthlySalary * 0.015,
-        sickness: monthlySalary * 0.0245,
-        accident: monthlySalary * 0.077662,
-        advance: monthlySalary * 0.0376,
-    };
-
-
-    console.log(socialSecurityContributions);
-
-
-    const countCOE = () => {
-        setCOE(monthlySalary * 2)
-    };
-
-    const countMC = () => {
-        setMC(monthlySalary * 4)
-    };
-
-    const countCW = () => {
-        setCW(monthlySalary * 6)
-    };
-
-    const countB2B = () => {
-        setB2B(monthlySalary * 8)
-    };
-
-    const handleButtonCLick = () => {
-        if (selectedOption) {
-            countCOE();
-            countMC();
-            countCW();
-            countB2B();
-        }
-    };
-
-
     return (
         <Wrapper>
             <Title>
