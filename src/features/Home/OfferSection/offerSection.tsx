8663b16cbfb024cc8619021624b4560bdd9725b4
Marcin Izdebski <mizdebski123@gmail.com>
2024-04-03 13:21:06 +0200

    Unused element from offer section removed

--- src/features/Home/OfferSection/offerSection.tsx
+++ src/features/Home/OfferSection/offerSection.tsx
@@ -1,35 +1,27 @@
 import React from 'react';
 import { Wrapper, OffersTitle, OfferWrapper, OfferTile, OfferTitle, OfferArticle, OfferLogo, OfferDetails, AllOffersButton } from './styledOfferSection';
 import { scrollTop } from '../../../core/scrollTop';
-import { useQuery } from '@tanstack/react-query';
-import { Loader } from '../../../common/Loader/loader';
-import { Error } from '../../../common/Error/error';
 import { OfferArray } from '../../../core/interface';
 
-export const OfferSection = () => {
-
-    const { data, isLoading, error } = useQuery({
-        queryKey: ['offers'],
-        queryFn: () => fetch("https://esay-employ-database-wfsz.onrender.com")
-            .then((response: Response) => response.json())
-    });
+interface OfferSectionProps {
+    data: OfferArray[];
+}
+export const OfferSection: React.FC<OfferSectionProps> = ({ data }) => {
 
     return (
         <Wrapper>
             <OffersTitle>Offer zone</OffersTitle>
-            {isLoading ? <Loader /> : error ? <Error /> : (
-                <OfferWrapper>
-                    {data.map((offer: OfferArray) => (
-                        <OfferTile to={`/Offer/${offer.id}`} key={offer.id}>
-                            <OfferLogo src={offer.logo} />
-                            <OfferArticle>
-                                <OfferTitle >{offer.position}</OfferTitle>
-                                <OfferDetails>{offer.company_name}</OfferDetails>
-                            </OfferArticle>
-                        </OfferTile>
-                    ))}
-                </OfferWrapper>
-            )}
+            <OfferWrapper>
+                {data.map((offer: OfferArray) => (
+                    <OfferTile to={`/Offer/${offer.id}`} key={offer.id}>
+                        <OfferLogo src={offer.logo} />
+                        <OfferArticle>
+                            <OfferTitle >{offer.position}</OfferTitle>
+                            <OfferDetails>{offer.company_name}</OfferDetails>
+                        </OfferArticle>
+                    </OfferTile>
+                ))}
+            </OfferWrapper>
             <AllOffersButton to='/Offers' onClick={() => scrollTop()}>All offers</AllOffersButton>
         </Wrapper>
     );
--- src/features/Home/home.tsx
+++ src/features/Home/home.tsx
@@ -3,18 +3,33 @@ import { CaptionNumber, Captions, Wrapper, Title, TitleWrapper } from './styledH
 import { OfferSection } from './OfferSection/offerSection';
 import { CarrerSection } from './CarrerSection/carrerSection';
 import { AppSection } from './AppSection/appSection';
+import { useQuery } from '@tanstack/react-query';
+import { Loader } from '../../common/Loader/loader';
+import { Error } from '../../common/Error/error';
 
 export const Home = () => {
 
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['offers'],
+        queryFn: () => fetch("https://esay-employ-database-wfsz.onrender.com")
+            .then((response: Response) => response.json())
+    });
+
+
     return (
         <Wrapper>
-            <TitleWrapper>
-                <Title>Easy Employ - Your Way to Find a Job</Title>
-                <Captions>Find your dream job in IT among <CaptionNumber>20 000</CaptionNumber> offers</Captions>
-            </TitleWrapper>
-            <OfferSection />
-            <CarrerSection />
-            <AppSection />
+            {isLoading ? <Loader /> : error ? <Error /> : (
+                <div>
+                    <TitleWrapper>
+                        <Title>Easy Employ - Your Way to Find a Job</Title>
+                        <Captions>Find your dream job in IT among <CaptionNumber>{data.length}</CaptionNumber> offers</Captions>
+                    </TitleWrapper>
+
+                    <OfferSection data={data} />
+                    <CarrerSection />
+                    <AppSection />
+                </div>
+            )}
         </Wrapper>
     );
 };
