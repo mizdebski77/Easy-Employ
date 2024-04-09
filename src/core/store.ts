794a4f4087dd4e040b36ce2dcd3552f1c046c1b3
Marcin Izdebski <mizdebski123@gmail.com>
2024-04-09 15:43:44 +0200

    navigation reducer to store added

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filtersReducer from '../features/JobOffers/Filters/filtersSlice';
-
+import navigationReducer from '../common/Navigation/MobileNavbar/navigationSlice';
 const store = configureStore({
     reducer: {
         filters: filtersReducer,
+        navigation: navigationReducer
     },
 });
 
